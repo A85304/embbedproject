f14dc69fdd6f13f3d2ca455732e1faabcda46056
mo3ta123 <84969331+mo3ta123@users.noreply.github.com>
2021-06-05 21:54:21 +0200

    Update Distance.h

--- Distance.h
+++ Distance.h
@@ -4,7 +4,7 @@
 #include "math.h"
 #include "stdint.h"
 #include"tm4c123gh6pm.h"
-void TURN_ON_MAX_DISTANCE(float distance,float max_distance); //turns on led if distance is more than max distance
+void TURN_ON(float distance,float max_distance); //turns on led if distance is more than max distance
 double distance(double X, double Y);// keeps track on distance total distance is returned
 #define pi 3.14159265358979323846
 
