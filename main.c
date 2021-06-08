970db5088cb5c0cb21f79f235d759b8b1553f387
mo3ta123 <84969331+mo3ta123@users.noreply.github.com>
2021-06-08 19:26:14 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,34 @@
-#include"tm4c123gh6pm.h"
-#include"Seven_Segment_Display.h"
-#include"UART_INI.h"
-#include"Distance.h"
-#include"GPIO_INI.h"
-#include"tm4c123.h"
+#include "tm4c123gh6pm.h"
+#include "Seven_Segment_Display.h"
+#include "UART_INI.h"
+#include "Distance.h"
+#include "GPIO_INI.h"
+#include "tm4c123.h"
+#include "LCD.h"
+#include "string.h"
+#include "GPS_RECIEVE.h"
+
+
 void SystemInit(){}
 	int main(void){
-		SCB->CPACR |=((3UL<<10*2)|(3UL<<11*2));
+		////////////////////////////////////////
+		//variables 
+		uint8_t *buffer;//gets data from gps
+		int i;
 		
-		//activating all needed peripherals
-		initportf();
-		initportA();
-    initportB();
-    initportD();
-    initportE();
-		//////////////////////////////////////////
-		Seven_Segment_Display(125);
-		TURN_ON_MAX_DISTANCE(125,100);
-		return 0;
+		
+		
+		///////////////////////////////////////////
+		//initialization
+		SCB->CPACR |=((3UL<<10*2)|(3UL<<11*2));
+		LCD_Init();
+		LCD_Command(lcd_SetCursor_1);
+		_delay_ms(2);
+		UART_INI();
+		
+		
+	///////////////////////////////////////////
+	//program
+	
+		
 }
-
