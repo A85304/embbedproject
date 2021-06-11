429502d9d5d3fc75672a23eed2e95d73d2b35874
Youssef6220 <85390510+Youssef6220@users.noreply.github.com>
2021-06-11 20:57:10 +0200

    Update Distance.c

--- Distance.c
+++ Distance.c
@@ -5,33 +5,38 @@
 #define pi 3.14159265358979323846
 
 
-float Total_Distance = 0;
+float Total_Distance = 0.0 ;
 double Latitude[2] = {0.0 , 0.0} ;
 double Longitude[2] = {0.0 , 0.0} ;
+double Lat1 = 0.0 ;
+double Long1 = 0.0 ;
 int f = 0 ; 
 
 void SystemInit(){}
 
-double deg2rad(double deg) {
+double deg2rad(float deg) {
+
     return (deg * pi / 180);
+
     }
 
-double rad2deg(double rad) {
+double rad2deg(float rad) {
+
         return (rad * 180 / pi);
+
     }
 
+void Distance( double Lat ,  double Long) {
+    double theta
+    double dist
 
-double Distance(double Lat, double Long) {
-		double theta;
-		double dist;
+	if (f == 1){
 
-		if 	(f == 1){
-
- 			Latitude[1] = Lat ;
-			Longitude[1] = Long ;           
+ 		Latitude[1] = Lat ;
+		Longitude[1] = Long ;           
 
             if((Latitude[0] == Latitude[1]) && (Longitude[0] == Longitude[1]) ){
-                return Total_Distance;
+
             }            
             theta = Longitude[0] - Longitude[1];
             dist = sin(deg2rad(Latitude[0]))*sin(deg2rad(Latitude[1]))+cos(deg2rad(Latitude[0]))*cos(deg2rad(Latitude[1]))*cos(deg2rad(theta));
@@ -47,10 +52,15 @@ double Distance(double Lat, double Long) {
 			Longitude[0] = Long ;
 		}
 
-		return Total_Distance;
-}
-int main(){}
-	
-	
 
+	}
+
+
+	void Gps_Reading(char *Gps_String){
+
+         char *Gps_recieve = Gps_String ;
+         char Points = sscanf_s(Gps_recieve, " %*c %*c %*c %*c %*c %*c %*c  %lf %*c %*c %*c %lf ",  &Lat1 , &Long1);
+
+        
+    }
            
