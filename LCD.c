0627468569103eabd29271509d1232051d409531
omar-cpu <60290041+omar-cpu@users.noreply.github.com>
2021-06-13 22:06:11 +0200

    Update LCD.c

--- LCD.c
+++ LCD.c
@@ -112,12 +112,8 @@ void LCD_Char(u8 data)
 }
 
 
-void LCD_String(u8* String, u8 Index,float distance)
 {
-	int dis=(int)distance;
-	int Units = (dis % 10);	
-	int Tenth = (dis % 100)/10;
-	int Hundereds = (dis % 1000)/100;
+
 	u8 iteration=0;
 
 	for (iteration=0; iteration< Index ; iteration++)
@@ -130,12 +126,7 @@ void LCD_String(u8* String, u8 Index,float distance)
 	}
 	
 	/////////////////////////
-	LCD_Char(Hundereds+'0');
-	_delay_ms(10);
-	LCD_Char(Tenth+'0');
-	_delay_ms(10);
-	LCD_Char(Units+'0');
-	_delay_ms(10);
+
 
 
 }
